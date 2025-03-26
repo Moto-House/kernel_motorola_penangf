1d2354c809f2aa30de3821b5f8ac9b3c9375adc4
R0rt1z2 <me@r0rt1z2.com>
2025-03-26 15:00:01 +0100

    lib: import rest of the OEM changes
    Signed-off-by: R0rt1z2 <me@r0rt1z2.com>

--- lib/dynamic_debug.c
+++ lib/dynamic_debug.c
@@ -260,11 +260,7 @@ static int ddebug_tokenize(char *buf, char *words[], int maxwords)
 		} else {
 			for (end = buf; *end && !isspace(*end); end++)
 				;
-			if (end == buf) {
-				pr_err("parse err after word:%d=%s\n", nwords,
-				       nwords ? words[nwords - 1] : "<none>");
-				return -EINVAL;
-			}
+			BUG_ON(end == buf);
 		}
 
 		/* `buf' is start of word, `end' is one past its end */
@@ -596,6 +592,14 @@ static char *dynamic_emit_prefix(const struct _ddebug *desc, char *buf)
 
 	return buf;
 }
+
+#if defined(CONFIG_MTK_PRINTK_DEBUG)
+void __dynamic_no_printk(struct _ddebug *descriptor, const char *fmt, ...)
+{
+
+}
+EXPORT_SYMBOL(__dynamic_no_printk);
+#endif
 
 void __dynamic_pr_debug(struct _ddebug *descriptor, const char *fmt, ...)
 {

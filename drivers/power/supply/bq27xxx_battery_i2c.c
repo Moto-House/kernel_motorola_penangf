4f992a692da2d234b51a7e7e8402a5b83355c58d
R0rt1z2 <me@r0rt1z2.com>
2025-03-26 14:59:31 +0100

    drivers/power: import OEM changes
    Signed-off-by: R0rt1z2 <me@r0rt1z2.com>

--- drivers/power/supply/bq27xxx_battery_i2c.c
+++ drivers/power/supply/bq27xxx_battery_i2c.c
@@ -162,7 +162,7 @@ static int bq27xxx_battery_i2c_probe(struct i2c_client *client,
 	di->id = num;
 	di->dev = &client->dev;
 	di->chip = id->driver_data;
-	di->name = name;
+	di->name = "battery";
 
 	di->bus.read = bq27xxx_battery_i2c_read;
 	di->bus.write = bq27xxx_battery_i2c_write;
@@ -209,9 +209,7 @@ static int bq27xxx_battery_i2c_remove(struct i2c_client *client)
 {
 	struct bq27xxx_device_info *di = i2c_get_clientdata(client);
 
-	if (client->irq)
-		free_irq(client->irq, di);
-
+	free_irq(client->irq, di);
 	bq27xxx_battery_teardown(di);
 
 	mutex_lock(&battery_mutex);
